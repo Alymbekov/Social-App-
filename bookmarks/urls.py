c9365575d6e90c54dabf7ee09545202b6dc0a659
Alymbekov <maxim.makarov.1997@mail.ru>
2020-03-11 21:00:23 +0600

    add reset password and change password logic

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('account/', include('account.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
